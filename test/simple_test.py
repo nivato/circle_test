19d9c0a88fd0e635109e1b4129468e3adc158aae
Nazar Ivato <nazarivato@gmail.com>
2019-07-01 14:47:01 +0300

    Setup Circle CI

---
+++ test/simple_test.py
@@ -0,0 +1,17 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class TestSomethingWorks(object):
+
+    def test_one(self):
+        logger.info('First test started')
+        x = "this"
+        assert 'h' in x
+
+    def test_two(self):
+        logger.info('Second test started')
+        x = "hello"
+        assert hasattr(x, 'check')
